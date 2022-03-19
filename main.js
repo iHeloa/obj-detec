3dd19e3720542f389870a3d1e98fb0b7d103a09b
iHeloa <82479317+iHeloa@users.noreply.github.com>
2022-03-19 11:03:37 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function start1(){
+    window.location=("song.html.html");
+}
+
+function start4(){
+    window.location=("trophy.html")
+}
+
+function start2(){
+    window.location=("pad.html")
+}
+
+function start3(){
+    window.location=("pika.html")
+}
+
+function start5(){
+    window.location("nintendo.html")
+}
